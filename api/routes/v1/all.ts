a640dbbf7a13d0bf087d68714440ac2c8ab6557e
sam <aquaticcalf@proton.me>
2026-01-12 10:46:35 +0530

    Rename interfaces and update property casing

--- api/routes/v1/all.ts
+++ api/routes/v1/all.ts
@@ -1,27 +1,27 @@
 import type { Elysia } from "elysia"
 
-interface openglowSession {
+interface openglowsession {
 	id: string
 	name: string
 	project: string
-	lastWorkedOn: string
-	isPRActive: boolean
+	lastworkedon: string
+	ispractive: boolean
 }
 
-interface getAllSessionsResponse {
+interface getallsessionsresponse {
 	sessions: openglowSession[]
 	total: number
 	page: number
-	perPage: number
+	perpage: number
 }
 
 export default (app: Elysia) =>
 	app.get(
 		"/v1/all",
-		({ query }): getAllSessionsResponse => ({
+		({ query }): getallsessionsresponse => ({
 			sessions: [],
 			total: 0,
 			page: Number(query.page) || 1,
-			perPage: Number(query.perPage) || 10,
+			perpage: Number(query.perPage) || 10,
 		}),
 	)
