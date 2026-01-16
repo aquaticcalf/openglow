1160224695d55c4764d9af5d950caf55cd2078d0
sam <aquaticcalf@proton.me>
2026-01-16 12:16:49 +0530

    empty

--- vite/app/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Index() {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-dvh relative px-4 py-6">
-			<h1 className="text-4xl font-bold">openglow</h1>
-		</div>
-	)
-}
